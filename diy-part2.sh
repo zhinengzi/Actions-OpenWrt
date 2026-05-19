cbef4e192524adc751e2eb428a0c85830ee70532
zhinengzi <qingpeng.cai@gmail.com>
2026-05-19 08:34:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,9 +20,9 @@
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 # 自动寻找 OpenWrt 当前使用的内核补丁目录
-PATCH_DIR=$(ls -d target/linux/x86/patches-* | tail -n 1)
+# PATCH_DIR=$(ls -d target/linux/x86/patches-* | tail -n 1)
 
-# # 直接从 Arch Linux 官方 AUR 仓库拉取适配了 6.x 新内核的 2.5G 补丁！
-curl -L "https://raw.githubusercontent.com/zhinengzi/linux6.x-bnx2x-2.5g-patch/refs/heads/main/linux6.x-bnx2x-2.5g.patch" -o $PATCH_DIR/999-linux6.x-bnx2x-2.5g.patch
+# 直接从 Arch Linux 官方 AUR 仓库拉取适配了 6.x 新内核的 2.5G 补丁！
+# curl -L "https://raw.githubusercontent.com/zhinengzi/linux6.x-bnx2x-2.5g-patch/refs/heads/main/linux6.x-bnx2x-2.5g.patch" -o $PATCH_DIR/999-linux6.x-bnx2x-2.5g.patch
 
-echo "===> [SUCCESS] Linux6.x-Bnx2x-2.5G 补丁已成功注入! <==="
+# echo "===> [SUCCESS] Linux6.x-Bnx2x-2.5G 补丁已成功注入! <==="
